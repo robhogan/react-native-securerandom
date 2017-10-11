2dad2dcae6c5190c8099017cbbe9cf0f26ed453e
rh389 <roberthogan@blueyonder.co.uk>
2017-10-11 19:00:59 +0100

    Use base64-js, add android support

--- android/src/main/java/net/rhogan/rnrandombytes/RNRandomBytesModule.java
+++ android/src/main/java/net/rhogan/rnrandombytes/RNRandomBytesModule.java
@@ -3,7 +3,10 @@ package net.rhogan.rnrandombytes;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+
+import java.security.SecureRandom;
+import android.util.Base64;
 
 public class RNRandomBytesModule extends ReactContextBaseJavaModule {
 
@@ -12,6 +15,14 @@ public class RNRandomBytesModule extends ReactContextBaseJavaModule {
   public RNRandomBytesModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+  }
+
+  @ReactMethod
+  public void generateRandomBytesAsBase64(int length, Promise promise) {
+    SecureRandom secureRandom = new SecureRandom();
+    byte[] buffer = new byte[length];
+    secureRandom.nextBytes(buffer);
+    promise.resolve(Base64.encodeToString(buffer, Base64.NO_WRAP));
   }
 
   @Override
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 /* @flow */
 
 import { NativeModules, Platform } from 'react-native';
+import { toByteArray } from 'base64-js';
 
 const { RNRandomBytes } = NativeModules;
 
 export function generateRandomBytes(length: number): Promise<Uint8Array> {
-    if (Platform.OS !== 'ios') {
-        throw Error('react-native-random-bytes is currently only available for iOS');
+    if (Platform.OS !== 'ios' && Platform.OS !== 'android') {
+        throw Error('react-native-random-bytes is currently only available for iOS and Android');
     }
 
     if (!RNRandomBytes || !RNRandomBytes.generateRandomBytesAsBase64) {
         throw Error('react-native-random-bytes is not properly linked');
     }
 
-    return RNRandomBytes.generateRandomBytesAsBase64(length).then(bytesAsString => {
-        return Uint8Array.from(atob(bytesAsString).split('').map(c => c.charCodeAt(0)));
-    })
+    return RNRandomBytes.generateRandomBytesAsBase64(length).then(base64 => toByteArray(base64));
 }
