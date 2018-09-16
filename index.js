af2ba718976b85bbe6b29147abd79fcd26a7b87d
Rob Hogan <rh389@users.noreply.github.com>
2018-09-16 16:22:27 +0100

    Remove platform constraints

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ import { toByteArray } from 'base64-js';
 const { RNSecureRandom } = NativeModules;
 
 export function generateSecureRandom(length: number): Promise<Uint8Array> {
-    if (Platform.OS !== 'ios' && Platform.OS !== 'android') {
-        throw Error('react-native-securerandom is currently only available for iOS and Android');
-    }
-
     if (!RNSecureRandom || !RNSecureRandom.generateSecureRandomAsBase64) {
         throw Error('react-native-securerandom is not properly linked');
     }
