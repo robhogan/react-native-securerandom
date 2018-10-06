83d2d783151c642a34202c7c601e3e64b0fd457d
rh389 <roberthogan@blueyonder.co.uk>
2018-10-06 18:29:22 +0100

    Don't mix synchronous throws with returning a Promise

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { RNSecureRandom } = NativeModules;
 
 export function generateSecureRandom(length) {
     if (!RNSecureRandom || !RNSecureRandom.generateSecureRandomAsBase64) {
-        throw Error('react-native-securerandom is not properly linked');
+        return Promise.reject(Error('react-native-securerandom is not properly linked'));
     }
 
     return RNSecureRandom.generateSecureRandomAsBase64(length).then(base64 => toByteArray(base64));
